33076f6c6a27cad724e68af0dc75241d9e7d4d79
jair <jair@jair-Latitude-E7240.home>
2021-07-08 18:00:11 +0100

    toward files diacritization

--- python/diacritize.py
+++ python/diacritize.py
@@ -21,20 +21,23 @@ def diacritization_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_kind", dest="model_kind", type=str, required=True)
     parser.add_argument("--config", dest="config", type=str, required=True)
-    parser.add_argument("--text", dest="text", type=str, required=True)
+    parser.add_argument("--text", dest="text", type=str, required=False)
+    parser.add_argument("--text_file", dest="text_file", type=str, required=False)
     return parser
 
 
 parser = diacritization_parser()
 args = parser.parse_args()
 
+if args.text is None and args.text_file is None:
+    raise ValueError("text or text_file params required!")
 
 if args.model_kind == "cbhg":
     diacritizer = CBHGDiacritizer(args.config, args.model_kind, 'log_dir')
-
 elif args.model_kind == "baseline":
     diacritizer = Seq2SeqDiacritizer(args.config, args.model_kind, 'log_dir')
 else:
     raise ValueError("The model kind is not supported")
 
+if 
 diacritizer.diacritize_text(args.text)
