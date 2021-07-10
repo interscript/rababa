669b84968ad762c07b7c037327897b7e80348f2a
jair <jair@jair-Latitude-E7240.home>
2021-07-10 14:21:05 +0100

    more deps to rewrite in ruby

--- lib/kithara/diacritizer.rb
+++ lib/kithara/diacritizer.rb
@@ -1,2 +1,4 @@
 this refers to:
   https://github.com/interscript/arabic-diacritization/blob/master/python/diacritizer.py
+as well as half of
+ https://github.com/almodhfer/Arabic_Diacritization/blob/master/config_manager.py
