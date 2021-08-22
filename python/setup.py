1b8eb1ec1641744f3a30d39dc040142c3420d5a8
Ronald Tse <ronald.tse@ribose.com>
2021-08-22 16:15:05 +0800

    Update Rababa python setup

--- python/setup.py
+++ python/setup.py
@@ -1,26 +1,46 @@
-from setuptools import setup, find_packages
+import re
+from os import environ
 
-setup(
+import setuptools
+
+with open("README.adoc", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+PKG_VERSION = "0.1.0"
+
+GIT_TAG = environ.get("GITHUB_REF", "")
+TAG_VERSION = re.match(r'^refs/tags/v([0-9]+\.[0-9a-z]+\.[0-9a-z]+)$', GIT_TAG)
+
+if TAG_VERSION:
+    PKG_VERSION = TAG_VERSION.group(1)
+
+setuptools.setup(
     name='rababa',
-    version='0.1.0',
+    version=PKG_VERSION,
+    author="Ribose",
+    author_email="open.source@ribose.com",
+    license='MIT',
     description='Rababa for Arabic diacriticization',
-    author='Ribose',
-    author_email='open.source@ribose.com',
+    # packages=['rababa'],
     url='https://www.interscript.org',
-    # packages=find_packages(include=['exampleproject', 'exampleproject.*']),
     python_requires='>=3.6, <4',
+    project_urls={
+        'Documentation': 'https://github.com/interscript/rababa',
+        'Source': 'https://github.com/interscript/rababa',
+        'Tracker': 'https://github.com/interscript/rababa/issues',
+    },
     install_requires=[
-      'torch==1.9.0',
-      'numpy==1.19.5',
-      'matplotlib==3.3.3',
-      'pandas==1.1.5',
-      'ruamel.yaml==0.16.12',
-      'tensorboard==2.4.0',
-      'diacritization-evaluation==0.5',
-      'tqdm==4.56.0',
-      'onnx==1.9.0',
-      'onnxruntime==1.8.1',
-      'pyyaml==5.4.1',
+      'torch>=1.9.0',
+      'numpy',
+      'matplotlib',
+      'pandas',
+      'ruamel.yaml',
+      'tensorboard',
+      'diacritization-evaluation',
+      'tqdm',
+      'onnx',
+      'onnxruntime',
+      'pyyaml',
     ],
     # extras_require={'plotting': ['matplotlib>=2.2.0', 'jupyter']},
     setup_requires=['pytest-runner'],
